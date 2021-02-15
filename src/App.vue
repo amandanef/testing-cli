863bdddbadfc44e2e801120b4c59f0cdba01a4a5
Amanda Nef <amandanef@mail.weber.edugit config --global user.email amandanef@mail.weber.edu>
2021-02-15 14:02:18 -0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>{{ title }}</h2>
   </div>
 </template>
 
@@ -9,9 +9,10 @@
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      title: 'Hello World'
+      }
   }
 }
 </script>
